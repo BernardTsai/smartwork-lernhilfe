015f5789a796f84833f620d6064ff06bb9cc23df
btsai <bernard@tsai.eu>
2020-04-06 14:27:00 +0200

    Adjusted exported port to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const parser  = require('body-parser')
 const fs      = require('fs')
 const app     = express()
-const port    = 3000
+const port    = 8080
 
 //------------------------------------------------------------------------------
 
