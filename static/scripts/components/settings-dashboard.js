57c4099179c55559f9cedc97667a77d706607efb
root <root@sw1.1>
2020-06-20 12:01:02 +0000

    added mask for password change

--- static/scripts/components/settings-dashboard.js
+++ static/scripts/components/settings-dashboard.js
@@ -2,16 +2,50 @@ Vue.component( 'settings-dashboard',
   {
     props:    ['model'],
     methods: {
-      select: function(index) {
-        model.profession = index
+      //select: function(index) {
+        //model.profession = index
 
-        model.mode = 'profession'
-      }
+        //model.mode = 'profession'
+      //}
     },
     template: `
       <div id="settings-dashboard" class="container">
 
-        <!-- loop over all professions -->
+        <h3 class="text-center">Konto verwalten</h3>
+
+        <div class="card my-3 mx-auto" style="max-width: 540px;" @click="this.model.submode='settings-password'">
+          <div class="row no-gutters">
+            <div class="col-md-2 my-auto">
+              <img src="images/logo.png" class="card-img p-1" alt="LOGO">
+            </div>
+            <div class="col-md-10">
+              <div class="card-body">
+                <h5 class="card-title">Passwort ändern</h5>
+                <p class="card-text">
+                  Hier kann das eigene Passwort geändert werden
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card my-3 mx-auto" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-2 my-auto">
+              <img src="images/logo.png" class="card-img p-1" alt="LOGO">
+            </div>
+            <div class="col-md-10">
+              <div class="card-body">
+                <h5 class="card-title">Ausbilderbereich</h5>
+                <p class="card-text">
+                  Verwaltung der Azubis, Erstellung neuer Benutzerkonten
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- loop over all professions
         <div v-for="(profession, index) in model.materials.professions" class="card my-3 mx-auto" style="max-width: 540px;" @click="select(index)">
           <div class="row no-gutters">
             <div class="col-md-2 my-auto">
@@ -19,7 +53,7 @@ Vue.component( 'settings-dashboard',
             </div>
             <div class="col-md-10">
               <div class="card-body">
-                <h5 class="card-title"><!--{{profession.title}}-->DASHBOARD CONTENT</h5>
+                <h5 class="card-title">{{profession.title}}</h5>
                 <p class="card-text">
                   {{profession.description}}
                 </p>
@@ -27,6 +61,7 @@ Vue.component( 'settings-dashboard',
             </div>
           </div>
         </div>
+        -->
 
       </div>`
   }
--- static/scripts/components/settings-password.js
+++ static/scripts/components/settings-password.js
@@ -2,31 +2,36 @@ Vue.component( 'settings-password',
   {
     props:    ['model'],
     methods: {
-      select: function(index) {
-        model.profession = index
+      //select: function(index) {
+        //model.profession = index
 
-        model.mode = 'profession'
-      }
+        //model.mode = 'profession'
+      //}
     },
     template: `
       <div id="settings-password" class="container">
 
-        <!-- loop over all professions -->
-        <div v-for="(profession, index) in model.materials.professions" class="card my-3 mx-auto" style="max-width: 540px;" @click="select(index)">
-          <div class="row no-gutters">
-            <div class="col-md-2 my-auto">
-              <img :src="'../../images/' + profession.image" class="card-img p-1" :alt="profession.profession">
-            </div>
-            <div class="col-md-10">
-              <div class="card-body">
-                <h5 class="card-title"><!--{{profession.title}}--> PASSWORT CONTENT</h5>
-                <p class="card-text">
-                  {{profession.description}}
-                </p>
-              </div>
-            </div>
+        <h3 class="text-center">Passwort ändern</h3>
+
+        <form class="mx-auto" style="max-width: 540px">
+          <div class="form-group">
+            <label for="inputOldPassword">Altes Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Altes Passwort">
+            <small id="oldPasswordHelp" class="form-text text-muted">Geben Sie Ihr altes Passwort ein.</small>
           </div>
-        </div>
+          <div class="form-group">
+            <label for="inputNewPassword1">Neues Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Neues Passwort">
+            <small id="newPasswordHelp" class="form-text text-muted">Geben Sie das neue Passwort ein.</small>
+          </div>
+          <div class="form-group">
+            <label for="inputNewPassword2">Neues Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Neues Passwort">
+            <small id="confirmNewPasswordHelp" class="form-text text-muted">Bestätigen Sie Ihr neues Passwort, indem Sie es erneut eingeben.</small>
+          </div>
+          <button type="submit" class="btn btn-primary">Bestätigen</button>
+        </form>
+
 
       </div>`
   }
